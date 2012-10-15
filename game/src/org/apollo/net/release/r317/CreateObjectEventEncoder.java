4ae5e11780547d83ca1f7674d7af174e77fe94a6
Steve Kreitzer <buroa@me.com>
2012-10-15 09:20:55 -0500

    Update game/src/org/apollo/net/release/r317/CreateObjectEventEncoder.java

--- game/src/org/apollo/net/release/r317/CreateObjectEventEncoder.java
+++ game/src/org/apollo/net/release/r317/CreateObjectEventEncoder.java
@@ -19,7 +19,7 @@ public final class CreateObjectEventEncoder extends EventEncoder<CreateObjectEve
 	public GamePacket encode(CreateObjectEvent event) {
 		final GameObject object = event.getObject();
 		final GamePacketBuilder builder = new GamePacketBuilder(151);
-		builder.put(DataType.BYTE, DataTransformation.ADD, 0);
+		builder.put(DataType.BYTE, DataTransformation.ADD, 0); // implement this accordingly
 		builder.put(DataType.SHORT, DataOrder.LITTLE, object.getDefinition().getId());
 		builder.put(DataType.BYTE, DataTransformation.SUBTRACT, (object.getType() << 2) + (object.getRotation() & 3));
 		return builder.toGamePacket();
